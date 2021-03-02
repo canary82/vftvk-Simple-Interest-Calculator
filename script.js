4a622a9487d7b009ca2361a87cfe788ea506bda7
Andrew Ignatenko <andr.ignatenko@gmail.com>
2021-03-02 09:32:41 +0000

    Updated JS

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest =  principal * years * rate / 100;
+    var year = new Date().getFullYear() + years;
+    document.getElementById('result').innerHTML = `If you deposit ${principal}, at an interest rate of ${rate}%. You will receive an amount of ${interest}, in the year ${year}`;
+
+}
+
+function updateSlider()
+{
+    var rate = document.getElementById("rate").value;
+    document.getElementById('slider_value').innerHTML = `${rate}%`;
 }
         
